37135b6b403a4d5d359f3209a02e8cdbc64b6221
Gabriel <email>
2022-06-18 12:09:06 -0300

    commiy_inicial

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="./css/estilo.css">
+
+</head>
+<body>
+    <main>
+      <div id="texto">
+        <h2>email</h2> 
+        <input type="text" class="parra">
+        <h2>senha</h2>
+        <input type="password" class="parra">
+      </div>
+      
+      <div id="botão">
+         <button>Login</button> 
+      </div>
+      
+    </main>
+
+    <script src="./js/compotamento.js"></script>
+
+</body>
+</html>
